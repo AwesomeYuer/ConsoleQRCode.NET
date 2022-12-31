f268cab4be75e89eae9ff5f496621bcb308f58a6
AwesomeYuer <AwesomeYuer@Microshaoft.com>
2022-12-31 16:22:38 +0800

    9

--- Sample/Program.cs
+++ Sample/Program.cs
@@ -10,30 +10,30 @@ Console.WriteLine("======");
 
 System.Console.Out.PrintQRCodeLine
             (
-                  data  : $@"AwesomeYuer于斯人也@Microshaoft
+                  data: $@"AwesomeYuer于斯人也@Microshaoft
                              AwesomeYuer于斯人也@Microshaoft"   //待生成二维码原始数据
-                    
-                , 10                                            //outputPostionLeft:    控制台二维码输出横向位置
-                , null!                                         //outputPostionTop:     控制台二维码输出纵向位置
 
-                , 2                                             //marginInPixel:        二维码图像生成边缘空白宽高度
+                , 10                                            //outputPostionLeft         :   控制台二维码输出横向位置
+                , null!                                         //outputPostionTop          :   控制台二维码输出纵向位置
 
-                , 30                                            //widthInPixel:         二维码图像生成宽度
-                , 30                                            //heightInPixel:        二维码图像生成高度
+                , 2                                             //marginInPixel             :   二维码图像生成边缘空白宽高度
 
-                , ConsoleColor.White                            //darkColor:            控制台二维码输出深颜色
-                , ConsoleColor.Red                              //lightColor:           控制台二维码输出浅颜色
+                , 30                                            //widthInPixel              :   二维码图像生成宽度
+                , 30                                            //heightInPixel             :   二维码图像生成高度
 
-                , nameof(Encoding.UTF8)                         //二维码字符集 支持中文不乱码
-                //, '囍'                                         //控制台二维码输出占位符 同时支持宽或窄字符, 窄: !@# , 宽: ㊚㊛囍♀♂♂♀☿♁⚢⚣⚤⚥⚦⚧⚨
+                , ConsoleColor.White                            //darkColor                 :   控制台二维码输出深颜色
+                , ConsoleColor.Red                              //lightColor                :   控制台二维码输出浅颜色
+
+                , nameof(Encoding.UTF8)                         //二维码字符集                :   utf-8 支持中文不乱码
+                //, '囍'                                         //控制台二维码输出占位符       :   同时支持宽或窄字符, 窄: !@# , 宽: ㊚㊛囍♀♂♂♀☿♁⚢⚣⚤⚥⚦⚧⚨
             );
 
 Console.Out.PrintQRCodeLine
                 (
                     "https://www.cnblogs.com/stulzq/p/14282461.html?Thanks"     //待生成二维码原始数据
-                    , 100                                                       //outputPostionLeft:    控制台二维码输出横向位置
-                    , 10                                                        //outputPostionTop:     控制台二维码输出纵向位置
-                    , placeholderChar: '♂'                                      //控制台二维码输出占位符 同时支持宽或窄字符, 窄: !@# , 宽: ㊚㊛囍♀♂♂♀☿♁⚢⚣⚤⚥⚦⚧⚨
+                    , 100                                                       //outputPostionLeft         :   控制台二维码输出横向位置
+                    , 10                                                        //outputPostionTop          :   控制台二维码输出纵向位置
+                    , placeholderChar: '♂'                                      //控制台二维码输出占位符       :   同时支持宽或窄字符, 窄: !@# , 宽: ㊚㊛囍♀♂♂♀☿♁⚢⚣⚤⚥⚦⚧⚨
                 );
 
 Console.WriteLine();
@@ -68,27 +68,27 @@ var s =
 Console.Out.PrintQRCodeLine
             (
                 s
-                , new Dictionary<EncodeHintType, object> ()
+                , new Dictionary<EncodeHintType, object>()
                     {
-                          { EncodeHintType.CHARACTER_SET            , nameof(Encoding.UTF8)                 }
-                        , { EncodeHintType.ERROR_CORRECTION         , "L"                                   }
-                        , { EncodeHintType.QR_COMPACT               , true                                  }
-                        , { EncodeHintType.PURE_BARCODE             , true                                  }
-                        //, { EncodeHintType.QR_VERSION               , 10                                    }
-                        //, { EncodeHintType.DISABLE_ECI              , true                                  }
-                        //, { EncodeHintType.GS1_FORMAT               , true                                  }
-                        , { EncodeHintType.MARGIN                   , 1                                     }
-                        //, { EncodeHintType.WIDTH                    , 15                                    }
-                        //, { EncodeHintType.HEIGHT                   , 15                                    }
+                          { EncodeHintType.CHARACTER_SET            , nameof(Encoding.UTF8) }   //编码字符集
+                        , { EncodeHintType.ERROR_CORRECTION         , "L"                   }   //编码纠错级别   : L, M, Q, H
+                        , { EncodeHintType.QR_COMPACT               , true                  }   //编码是否压缩
+                        , { EncodeHintType.PURE_BARCODE             , true                  }   //编码是否纯条码
+                        //, { EncodeHintType.QR_VERSION               , 10                  }   //编码版本
+                        //, { EncodeHintType.DISABLE_ECI              , true                }   //编码是否禁用ECI编码段
+                        //, { EncodeHintType.GS1_FORMAT               , true                }   //编码是否GS1格式
+                        , { EncodeHintType.MARGIN                   , 3                     }
+                        //, { EncodeHintType.WIDTH                    , 15                  }
+                        //, { EncodeHintType.HEIGHT                   , 15                  }
                     }
-                
-                , 20                                                    //outputPostionLeft:    控制台二维码输出横向位置
-                , darkColor                 : ConsoleColor.Yellow
-                , lightColor                : ConsoleColor.DarkBlue
-                , placeholderChar           : '$'                       //控制台二维码输出占位符 窄字符 $
+
+                , 20                                                //outputPostionLeft     :   控制台二维码输出横向位置
+                , darkColor             : ConsoleColor.Yellow
+                , lightColor            : ConsoleColor.DarkBlue
+                , placeholderChar       : '$'                       //控制台二维码输出占位符   :   窄字符 $
             );
 
 Console.Out.PrintQRCodeLine
             (
-                $@"AwesomeYuer于斯人也" //待生成二维码原始数据
+                $@"AwesomeYuer于斯人也"    //待生成二维码原始数据
             );
